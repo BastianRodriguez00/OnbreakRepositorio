b79dfce870765dcb60264501845494f92bd6c0bc
SatabasCTM <57550408+SatabasCTM@users.noreply.github.com>
2020-04-05 21:34:34 -0400

    avance

--- Onbreak/Onbreak/GestionCliente.xaml.cs
+++ Onbreak/Onbreak/GestionCliente.xaml.cs
@@ -23,5 +23,10 @@ namespace Onbreak
         {
             InitializeComponent();
         }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
