1ccbe46b019edce141e5d73d1a21dbab6ca88581
Angel Vinet Mena <an.vinet@alumnos.duoc.cl>
2020-04-13 12:17:58 -0400

    seguido

---
+++ Onbreak/Onbreak/GestionContratosEventos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Onbreak
+{
+    /// <summary>
+    /// Lógica de interacción para Gestion_de_Contratos_de_Eventos.xaml
+    /// </summary>
+    public partial class Gestion_de_Contratos_de_Eventos : Window
+    {
+        public Gestion_de_Contratos_de_Eventos()
+        {
+            InitializeComponent();
+        }
+    }
+}
